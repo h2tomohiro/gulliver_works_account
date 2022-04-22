adc4820ff054ed6509dfd439792b4b6f419add62
h2tomohiro <h2.adachi.tomohiro@gmail.com>
2022-04-21 19:26:23 -0700

    add only jsx in LoginPage

--- src/scenes/signIn/index.tsx
+++ src/scenes/signIn/index.tsx
@@ -18,14 +18,31 @@ const SignInPage = () => {
   const onSubmit = (data: SignInParams) => {
     signIn(data);
   };
+
   return (
     <div className={styles.page}>
-      <form onSubmit={handleSubmit(onSubmit)}>
-        <input name="account.email" ref={register} />
-        <input name="account.password" ref={register} />
-        <button>ログイン</button>
-        <Link to="/">ホームへ</Link>
-      </form>
+      <div className={styles.loginContainer}>
+        <h1>求職者ログイン</h1>
+        <form onSubmit={handleSubmit(onSubmit)}>
+          <div className={styles.emailInputWrapper}>
+            <p>メールアドレス</p>
+            <input name="account.email" ref={register} />
+          </div>
+          <div className={styles.passwordInputWrapper}>
+            <p>パスワード</p>
+            <input name="account.password" ref={register} />
+          </div>
+          <div className={styles.loginButtonWrapper}>
+            <button>ログイン</button>
+          </div>
+        </form>
+        <div className={styles.linkToForgetPasswordWrapper}>
+          <Link to="/">パスワードを忘れた方はこちら</Link>
+        </div>
+        <div className={styles.signupWrapper}>
+          <button>新規登録はこちら</button>
+        </div>
+      </div>
     </div>
   );
 };
