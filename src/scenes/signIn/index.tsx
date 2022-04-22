c8dc5b0c69b9e5878ef033bda608914110f34f91
h2tomohiro <h2.adachi.tomohiro@gmail.com>
2022-04-22 00:18:33 -0700

    use hookform/error-message

--- src/scenes/signIn/index.tsx
+++ src/scenes/signIn/index.tsx
@@ -1,12 +1,13 @@
 import React, { useEffect } from "react";
 import { useForm } from "react-hook-form";
-import { Link, useHistory } from "react-router-dom";
+import { ErrorMessage } from '@hookform/error-message';
+import { useHistory } from "react-router-dom";
 import { useCurrentAccount } from "../../hooks/useCurrentAccount";
 import styles from "./style.module.scss";
 import { SignInParams, useSignInPresenter } from "./useSignInPresenter";
 
 const SignInPage = () => {
-  const { register, handleSubmit } = useForm<SignInParams>();
+  const { register, handleSubmit, errors } = useForm<SignInParams>();
   const { signIn } = useSignInPresenter();
   const { account } = useCurrentAccount();
   const history = useHistory();
@@ -30,9 +31,18 @@ const SignInPage = () => {
               className={styles.input}
               type="email"
               name="account.email"
-              ref={register}
               placeholder="coadmap@mail.com"
+              ref={
+                register({
+                  required: true,
+                  pattern: {
+                    value: /^[a-zA-Z0-9.!#$%&'*+\/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/,
+                    message: 'メールアドレスの形式が正しくありません。',
+                  },
+                })
+              }
               />
+            <ErrorMessage errors={errors} name="email" />
           </div>
           <div className={styles.passwordInputWrapper}>
             <p>パスワード</p>
@@ -40,15 +50,29 @@ const SignInPage = () => {
               className={styles.input}
               type='password'
               name="account.password"
-              ref={register}
               placeholder="パスワードを入力"
+              ref={
+                register({
+                  required: true,
+                  minLength: {
+                    value: 6,
+                    message: "パスワードは6文字以上で入力してください。"
+                  },
+                  pattern: {
+                    value: /^[a-zA-Z0-9!-/:-@¥[-`{-~]*$/,
+                    message: 'パスワードは半角英数記号のみ設定できます。',
+                  },
+                })
+              }
               />
+            <ErrorMessage errors={errors} name="password" />
           </div>
           <div className={styles.loginButtonWrapper}>
             <button className={styles.button}>ログイン</button>
           </div>
         </form>
         <div className={styles.forgetPasswordWrapper}>
+        { /* TODO: リンク設定 */}
           <a href="">パスワードを忘れた方はこちら</a>
         </div>
         <div className={styles.signupWrapper}>
